9535ef06a7211081ad551fcdc164f3d7b6535e3e
Bartosz <nasieniu@gmail.com>
2022-09-01 20:52:12 +0200

    landig Pege Style Netflix

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Header from "./features/header/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+export default App;
---
+++ src/features/header/header.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function Header() {
+  return (
+    <>
+      <div>h</div>
+    </>
+  );
+}
+
+export default Header;
